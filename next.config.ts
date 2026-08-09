c164d9993f87814ca8fa3e2ef66196b6c4193bb5
Bishal Baniya <mr.bishal.baniya@gmail.com>
2026-08-09 18:58:40 +0545

    perf: Reduce image quality to save Cloudinary bandwidth (60-80 instead of 75-90)

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     minimumCacheTTL: 60,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    qualities: [75, 80, 85, 90],
+    qualities: [60, 70, 75, 80], // Reduced quality to save Cloudinary bandwidth
     remotePatterns: [
       {
         protocol: "https",
